735e8f8e2d53df3b5bf49eaed7226a190b0ddea8
md-jihad-khan <mdjihadkhan9867@gmail.com>
2024-06-02 13:18:37 +0600

    created the jwt api

--- index.js
+++ index.js
@@ -25,6 +25,15 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // jwt
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "6h",
+      });
+      res.send({ token });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
